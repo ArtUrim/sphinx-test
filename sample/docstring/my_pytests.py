7bda9aff152df63f445f371306816a4aa723b46d
Artur Łoziński <artur.lozinski@fideltronik.com>
2022-07-06 21:40:21 +0200

    example embedded in the pytest script

---
+++ sample/docstring/my_pytests.py
@@ -0,0 +1,35 @@
+def test_sth():
+   """:reqs: abcd-4,abdc-2
+
+   to jest test
+
+   :suite: nightly
+   :init: tutu
+
+   :passcrit: działa
+
+   :desc: jakoś
+
+   :step: krok 1
+   :step: krok 2
+   :step: krok 3
+   """
+   assert 1
+
+def test_anything():
+   """
+   :reqs: abcd-3
+
+   to jest test
+
+   :init: tutu
+
+   :passcrit: działa
+
+   :desc: jakoś
+
+   :step: krok 1
+   :step: krok 2
+   :step: krok 3
+   """
+   assert 2 > 1
