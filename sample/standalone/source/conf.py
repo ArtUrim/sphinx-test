a76d39fc9a88d6ea8bd7f55ffba0bbd6dfd4e199
Artur Łoziński <lozinski.artur@gmail.com>
2022-08-23 19:19:38 +0200

    prepare for pypi

--- sample/standalone/source/conf.py
+++ sample/standalone/source/conf.py
@@ -10,11 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.append( os.path.abspath('..'))
-sys.path.append( os.path.abspath('../../../ext'))
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
@@ -32,7 +30,7 @@ release = '0.1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [ "tests"
+extensions = [ "sphinx_test_cases.tests"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
